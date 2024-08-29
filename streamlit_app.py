137b96cfd77f0be883451717c16b95d913837285
mohamed khaled <115542866+Mohamed12Khaled@users.noreply.github.com>
2024-08-29 23:59:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ st.title('ML app')
 st.info('The deployment')
 with st.expander("Data"):
   st.write("**Raw Data**")
-df = pd.read_csv("https://raw.githubusercontent.com/Mohamed12Khaled/dp-ml/master/PlayStore_Apps.csv")
-df
+  df = pd.read_csv("https://raw.githubusercontent.com/Mohamed12Khaled/dp-ml/master/PlayStore_Apps.csv")
+  df

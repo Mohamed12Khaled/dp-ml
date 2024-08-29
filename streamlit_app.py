d08d340cb13ba35747b334297a823c240d41ebdc
mohamed khaled <115542866+Mohamed12Khaled@users.noreply.github.com>
2024-08-29 23:53:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import seaborn as sns
 st.title('ML app')
 
 st.info('The deployment')
+df = pd.read_csv("https://raw.githubusercontent.com/Mohamed12Khaled/dp-ml/master/PlayStore_Apps.csv")

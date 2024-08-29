fa500f6b1695866a5eeb8b8f122021a3d50500ae
mohamed khaled <115542866+Mohamed12Khaled@users.noreply.github.com>
2024-08-30 00:08:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title('ML app')
 st.info('The deployment')
 with st.expander("Data"):
   st.write("**Raw Data**")
-  df = pd.read_csv("https://raw.githubusercontent.com/Mohamed12Khaled/dp-ml/master/PlayStore_Apps.csv")
+  df = pd.read_csv("https://raw.githubusercontent.com/Mohamed12Khaled/dp-ml/master/PlayStore_Apps.csv" , na_values=['Varies with device'])
   df
   st.write('**X**')
   X_raw = df.drop('Installs', axis=1)
@@ -16,3 +16,11 @@ with st.expander("Data"):
   st.write('**y**')
   y_raw = df.Installs
   y_raw
+with st.expander("Model"):
+  st.write("**learning and cleaning**")
+  null_vals = pd.DataFrame(df.isnull().mean() * 100, columns= ['Percent of Null Values'])
+  null_vals = null_vals[null_vals['Percent of Null Values'] > 0]
+  null_vals
+  null = null_vals[null_vals['Percent of Null Values'] > 25]
+  print(len(null))
+  df= df.drop(null.index, axis = 1)
